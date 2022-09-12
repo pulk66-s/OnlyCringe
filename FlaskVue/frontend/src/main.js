49c52d938e9489c0b3c5eb7000a3cfe9fcef9d4f
pulk66-s <hugo.marrasse@epitech.eu>
2022-09-12 15:39:34 +0200

    [ADD] Adding of home Page

--- FlaskVue/frontend/src/main.js
+++ FlaskVue/frontend/src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as VueRouter from 'vue-router'
+import HomePage from "./pages/HomePage.vue"
 
-createApp(App).mount('#app')
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: HomePage
+        },
+    ]
+})
+
+const app = createApp(App)
+app.use(router).mount('#app')
+app.mount('app')
---
+++ FlaskVue/frontend/src/pages/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>DIV</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    data() {
+        return (
+            {}
+        );
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+@import './HomePage.css';
+</style>
