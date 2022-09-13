ac8391c9ecf9ba6454bcf24f01158d7ea0bb62c2
pulk66-s <hugo.marrasse@epitech.eu>
2022-09-14 00:01:54 +0200

    [ADD] Adding of echo comment into prodSetup.sh

--- FlaskVue/frontend/scripts/prodSetup.sh
+++ FlaskVue/frontend/scripts/prodSetup.sh
@@ -1,5 +1,12 @@
+echo "DELETING of old frontend folder"
 sudo rm -rf /var/www/onlycringe/frontend
+echo "copying folder from home to /var/www/onlycringe"
 sudo cp -r ~/frontend /var/www/onlycringe
+echo "changing dir"
 cd /var/www/onlycringe/frontend
+echo "installing"
 sudo npm install
+echo "restart onlycringeFrontend.service"
 sudo systemctl restart onlycringeFrontend.service
+echo "DONE, status:"
+sudo systemctl status onlycringeFrontend.service
