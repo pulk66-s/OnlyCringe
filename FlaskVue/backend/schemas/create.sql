698726ad24cb37e6b83a4e323f2bf170fa5bc725
pulk66-s <hugo.marrasse@epitech.eu>
2022-09-14 20:03:37 +0200

    [ADD] Adding of topics and posts schemas

--- FlaskVue/backend/schemas/create.sql
+++ FlaskVue/backend/schemas/create.sql
@@ -8,3 +8,20 @@ create table User (
     password text not null,
     email text not null unique
 );
+
+create table Topics (
+    uuid UUID primary key not null default UUID(),
+    name text not null unique,
+    author_uuid UUID not null,
+    foreign key (author_uuid) references User(uuid)
+);
+
+create table Post (
+    uuid UUID primary key not null default UUID(),
+    title text not null,
+    content text not null,
+    author_uuid UUID not null,
+    topic_uuid UUID not null,
+    foreign key (author_uuid) references User(uuid),
+    foreign key (topic_uuid) references Topics(uuid)
+);
